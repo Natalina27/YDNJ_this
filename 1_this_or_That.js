7333cf2af154d8d39239916c82582fd43d9dcfd5
Natalya Myunster <natalyamyunster@yahoo.com>
2019-12-18 20:56:10 +0100

    foo.count

--- 1_this_or_That.js
+++ 1_this_or_That.js
@@ -1,3 +1,5 @@
+
+//1.
 function identify() {
     return this.name.toUpperCase();
 }
@@ -20,5 +22,23 @@ identify.call(you);
 
 speak.call(me); //Hello, I'm DASHA
 speak.call(you); //Hello, I'm MISHA
+
+//2.
+
+function foo(num) {
+    console.log('foo: ' + num);
+    this.count++;
+  //  console.log(this );
+}
+
+foo.count = 0;
+
+for(let i = 0; i < 10; i++){
+    if(i > 5){
+        foo(i);
+    }
+}
+console.log(foo.count );
+
 
 
