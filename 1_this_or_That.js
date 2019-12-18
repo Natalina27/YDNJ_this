4870671c9b8bb7a1e2bb0c2b1b2c5b7ca7e0ab3d
Natalya Myunster <natalyamyunster@yahoo.com>
2019-12-18 16:38:11 +0100

    2 objs and invoke identify.call and speak.call

--- 1_this_or_That.js
+++ 1_this_or_That.js
@@ -1,8 +1,26 @@
 function identify() {
     return this.name.toUpperCase();
+
+
 }
 
 function speak() {
     let greeting = 'Hello, I\'m ' + identify.call(this);
     console.log(greeting);
 }
+
+const me ={
+    name: 'Dasha',
+};
+
+const you ={
+    name: 'Misha',
+};
+
+identify.call(me);
+identify.call(you);
+
+speak.call(me);
+speak.call(you);
+
+
